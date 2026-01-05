3581e10f491d401a711c3a33b23d58142b118ed0
Luuk <147055722+Lutrian1@users.noreply.github.com>
2026-01-05 14:19:43 +0100

    Titel veranderd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>STATIC SITE GENERATION, LES VAN JUSTUS!!!!!</h1>
+
